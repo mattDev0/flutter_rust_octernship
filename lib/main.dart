0153994a2058c4dff7c1ea9477593d638c448e1f
mattDev0 <amatthew642@gmail.com>
2023-06-13 03:41:36 +0100

    flutter: create a future list and prompt to get password from user

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<String>> _futureList;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureList = _fetchList();
+  }
+
+  // fetch list of files
+  Future<List<String>> _fetchList([String? password]) async {
+    try {
+      // try with polkit first
+      return await api.lsWithPolkit();
+    } catch (_) {
+      // if polkit fails, try with sudo
+      password = await _requestPassword(context);
+      return await api.lsWithSudo(password: password);
+    }
+  }
+
+  Future<String> _requestPassword(BuildContext context) {
+    // Create a text controller and use it to retrieve the current value
+    TextEditingController passwordController = TextEditingController();
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Permission denied'),
+          content: TextField(
+            controller: passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(hintText: "Enter your password"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(passwordController.text);
+              },
+            ),
+          ],
+        );
+      },
+      // return the password
+    ).then((value) => Future<String>.value(value as String));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

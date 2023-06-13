1a085a97a518a19a7b301862c24b5acd145fdfb2
mattDev0 <amatthew642@gmail.com>
2023-06-13 03:32:42 +0100

    flutter: clean up home page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Elevate priviledge to run a Linux command'),
+      home:
+          const MyHomePage(title: 'Elevate priviledge to run a Linux command'),
     );
   }
 }
@@ -34,15 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("ls -la /root/", style: TextStyle(fontSize: 40.0)),
-            const Text('Run above cmd with Rust and print the output here'),
-          ],
-        ),
       ),
     );
   }
